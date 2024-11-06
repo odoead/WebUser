3884709cdde35a3f118780677b514523e7f53b06
Kirill <muravjov.kirill@gmail.com>
2024-11-06 18:56:27 +0200

    applied User reqired in Cart migration

--- WebUser/Migrations/20241106165517_setUserInCartAsRequired.Designer.cs
+++ WebUser/Migrations/20241106165517_setUserInCartAsRequired.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebUser.Data;
 
@@ -11,9 +12,10 @@ using WebUser.Data;
 namespace WebUser.Migrations
 {
     [DbContext(typeof(DB_Context))]
-    partial class DB_ContextModelSnapshot : ModelSnapshot
+    [Migration("20241106165517_setUserInCartAsRequired")]
+    partial class setUserInCartAsRequired
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
